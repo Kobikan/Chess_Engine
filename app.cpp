982898f6ec02ecccf7410a54b640a6b3310a36c4
Kobikan <kobikan.linganathan@gmail.com>
2020-01-02 03:18:32 -0500

    C++ formatting

--- app.cpp
+++ app.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <string>
 #include <iterator>
-#include <queue>
 #include <map>
+#include <queue>
+#include <string>
 
 using namespace std;
 
-queue<string> setup(){
-  queue <string> pieces;
-  string high[] = {"rook","knight","bishop", "queen", "king","bishop","knight","rook"};
+queue<string> setup() {
+  queue<string> pieces;
+  string high[] = {"rook", "knight", "bishop", "queen",
+                   "king", "bishop", "knight", "rook"};
 
-  for(string& i : high){
+  for (string &i : high) {
     pieces.push(i);
   }
   return pieces;
 }
 
-map<int, string> move(map<int, string> board){
+map<int, string> move(map<int, string> board) {
   int start;
   int end;
   cin >> start;
@@ -26,38 +27,36 @@ map<int, string> move(map<int, string> board){
   return board;
 }
 
-
-int main(){
+int main() {
   queue<string> pieces = setup();
   map<int, string> board;
   for (int i = 1; i <= 64; i++) {
-    if( i <= 8 || i >= 57){
+    if (i <= 8 || i >= 57) {
       pieces.push(pieces.front());
       board.insert(pair<int, string>(i, pieces.front()));
       pieces.pop();
-    } else if(i <= 16 || (i >= 49 && i <= 56)){
+    } else if (i <= 16 || (i >= 49 && i <= 56)) {
       board.insert(pair<int, string>(i, "pawn"));
     } else {
       board.insert(pair<int, string>(i, "[]"));
     }
-}
-
-
-map<int, string>::iterator itr;
-for (itr = board.begin(); itr != board.end(); ++itr) {
-  cout  << itr->second << ' ';
-  if( itr->first % 8 == 0){
-    cout << '\n';
   }
-}
-while(1){
-board = move(board);
-for (itr = board.begin(); itr != board.end(); ++itr) {
-  cout  << itr->second << ' ';
-  if( itr->first % 8 == 0){
-    cout << '\n';
+
+  map<int, string>::iterator itr;
+  for (itr = board.begin(); itr != board.end(); ++itr) {
+    cout << itr->second << ' ';
+    if (itr->first % 8 == 0) {
+      cout << '\n';
+    }
   }
-}
-}
-    return 0;
+  while (1) {
+    board = move(board);
+    for (itr = board.begin(); itr != board.end(); ++itr) {
+      cout << itr->second << ' ';
+      if (itr->first % 8 == 0) {
+        cout << '\n';
+      }
+    }
+  }
+  return 0;
 }
